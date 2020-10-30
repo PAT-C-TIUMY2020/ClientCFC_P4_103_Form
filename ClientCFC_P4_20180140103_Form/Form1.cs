deb73d472e6cc12932f484d20401622f0005cb43
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-10-30 18:41:57 +0700

    Add project files.

---
+++ ClientCFC_P4_20180140103_Form/Form1.cs
@@ -0,0 +1,52 @@
+﻿using _ServiceMtk_P1_20180140103;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static _ServiceMtk_P1_20180140103.IMatematika;
+
+namespace ClientCFC_P4_20180140103_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            label1.Text = "1 + 2 = " + hasilTambah;
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            label2.Text = "3 - 2 = " + hasilKurang;
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            label3.Text = "2 x 2 = " + hasilKali;
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            label4.Text = "2 / 2 = " + hasilBagi;
+            Console.ReadLine();
+
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
---
+++ ClientCFC_P4_20180140103_Form/Program.cs
@@ -0,0 +1,40 @@
+﻿using _ServiceMtk_P1_20180140103;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static _ServiceMtk_P1_20180140103.IMatematika;
+
+namespace ClientCFC_P4_20180140103_Form
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
